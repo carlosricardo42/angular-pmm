82dc2e61474c1a9214cd0e2792748f4bdbcbffba
Ricardo Mora <carlosricardo42@gmail.com>
2018-06-20 09:47:15 -0600

    path dist server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/angular-pmm'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/angular-pmm/index.html'));
 });
